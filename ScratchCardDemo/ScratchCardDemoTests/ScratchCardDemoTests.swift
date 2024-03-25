c0a4b254ccf7fd2995be4f2f617a51496877af2c
Anton Brinda <anton.brinda@nordlicht-it.com>
2024-03-25 16:33:19 +0100

    test

--- ScratchCardDemo/ScratchCardDemoTests/ScratchCardDemoTests.swift
+++ ScratchCardDemo/ScratchCardDemoTests/ScratchCardDemoTests.swift
@@ -11,6 +11,7 @@ import SwiftData
 final class ScratchCardDemoTests: XCTestCase {
 
     override func setUpWithError() throws {
+        
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
